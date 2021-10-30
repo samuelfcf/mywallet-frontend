5836720d86157ca8aefebbf073affea3c6966fb5
Samuel Felipe <samuelfelipef@gmail.com>
2021-10-30 16:11:58 -0300

    update base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://mywallet-backendd.herokuapp.com/";
+const BASE_URL = "https://mywallet-backendd.herokuapp.com";
 
 const getConfig = (token) => {
   return {
