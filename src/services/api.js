c1d4fa88857c3a740e24f280455eabd5794dd672
Samuel Felipe <samuelfelipef@gmail.com>
2021-11-27 14:52:56 -0300

    added environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000";
-
-//https://mywallet-backendd.herokuapp.com
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+console.log(BASE_URL);
 
 const getConfig = (token) => {
   return {
